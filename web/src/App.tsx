c936316d3d2c2f8bbf2f44561b520fbe8120d205
Miguel <miguelsousalima@hotmail.com>
2020-08-03 18:44:09 -0300

    Landing Page Mobile first

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Landing from './Pages/Landing';
+
+import './assets/styles/global.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Next Level Week</h1>
-    </div>
+    <Landing />
   );
 }
 
